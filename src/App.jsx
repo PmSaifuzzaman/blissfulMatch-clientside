4a24530ff6f3e974463814797cb535122a1af502
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-23 23:17:18 +0600

    React router, Tailwind CSS and material UI added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import {
+  Button,
+} from "@material-tailwind/react";
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1 className='text-4xl text-red-600 text-center'>This is my website</h1>
+      <Button>Button</Button>
+      
+    </>
+  )
+}
+
+export default App
