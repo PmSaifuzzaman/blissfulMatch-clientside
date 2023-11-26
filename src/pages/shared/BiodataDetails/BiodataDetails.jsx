590b4d84454afb77088c47e0343c62857a393f56
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-26 21:04:08 +0600

    Details page created

--- src/pages/shared/BiodataDetails/BiodataDetails.jsx
+++ src/pages/shared/BiodataDetails/BiodataDetails.jsx
@@ -1,3 +1,12 @@
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+
 import { useLoaderData } from "react-router-dom";
 import Navigationbar from "../../../components/Navbar/Navbar";
 
@@ -5,16 +14,122 @@ import Navigationbar from "../../../components/Navbar/Navbar";
 const BiodataDetails = () => {
 
     const singleBiodata = useLoaderData()
-    console.log(singleBiodata)
+
+    const { ProfileImage, Biodata, Occupation, BiodataNumber, Age, PermanentDivisionName, FathersName, MothersName, Name, DateOfBirth, Height, Weight, Race, PresentDivisionName, ExpectedPartnerAge, ExpectedPartnerHeight, ExpectedPartnerWeight } = singleBiodata;
 
     return (
         <div>
             <Navigationbar></Navigationbar>
             <div className="flex flex-row">
-                <div className="basis-3/4 h-96 bg-green-500">
+                <div className="basis-3/5 ">
+                    <Card className="">
+                        <CardHeader shadow={false} floated={false} className="h-96">
+                            <img
+                                src={ProfileImage}
+                                alt="card-image"
+                                className="h-full w-full object-contain"
+                            />
+                        </CardHeader>
+                        <CardBody>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Id : <span className="font-bold">{BiodataNumber}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Name : <span className="font-bold">{Name}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium my-2">
+                                    <span className="font-bold text-pink-400">{Biodata}</span>
+                                </Typography>
 
+                            </div>
+                            <div className=" flex items-center justify-between">
+                                <Typography color="blue-gray" className="font-medium">
+                                    Date of Birth : <span className="font-bold">{DateOfBirth}</span>
+                                </Typography>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Age : <span className="bg-pink-200 p-1 rounded-full text-black font-bold">{Age}</span>
+                                </Typography>
+                            </div>
+                            <div className=" flex items-center justify-between">
+                                <Typography color="blue-gray" className="font-medium">
+                                    Height : <span className="font-bold ">{Height}</span>
+                                </Typography>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Weight : <span className=" text-black font-bold">{Weight}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Fathers Name : <span className="font-bold">{FathersName}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Mothers Name : <span className="font-bold">{MothersName}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Race : <span className="font-bold">{Race}</span>
+                                </Typography>
+                            </div>
+                            <Typography
+                                color="gray"
+                                className=""
+                            >
+                                Occupation : <span className="bg-pink-200 p-1 rounded-full text-white">{Occupation}</span>
+                            </Typography>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Permanent Address : <span className="font-bold">{PermanentDivisionName}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Present Address : <span className="font-bold">{PresentDivisionName}</span>
+                                </Typography>
+                            </div>
+                            <div className=" flex items-center justify-between">
+                                <Typography color="blue-gray" className="font-medium">
+                                    Expected Partner Height : <span className="font-bold ">{ExpectedPartnerHeight}</span>
+                                </Typography>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Expected Weight : <span className=" text-black font-bold">{ExpectedPartnerWeight}</span>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography color="blue-gray" className="font-medium">
+                                    Partner Expected Age : <span className="font-bold bg-pink-300  rounded-full px-1">{ExpectedPartnerAge}</span>
+                                </Typography>
+                            </div>
+                        </CardBody>
+                        <CardFooter className="pt-0">
+                            <div>
+                            <Button
+                                ripple={false}
+                                
+                                className="bg-pink-400 text-white shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                            >
+                                Request
+                            </Button>
+                            <Button
+                                ripple={false}
+                                
+                                className="bg-pink-400 text-white shadow-none hover:scale-105 hover:shadow-none focus:scale-105 focus:shadow-none active:scale-100"
+                            >
+                                Request
+                            </Button>
+                            </div>
+                            
+                        </CardFooter>
+                    </Card>
                 </div>
-                <div className="basis-1/4 bg-red-400">
+                <div className="basis-2/5 bg-red-400">
 
                 </div>
 
