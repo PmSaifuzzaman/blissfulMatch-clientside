00c3aebb2e65dc32cdfbab877f222eb8d415a1db
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-26 13:20:53 +0600

    Firebase app created successfully

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdPrnv90RgHf2DyDv-bMbfzY_ZR6ifRLE",
+  authDomain: "blissfulmatch-31a41.firebaseapp.com",
+  projectId: "blissfulmatch-31a41",
+  storageBucket: "blissfulmatch-31a41.appspot.com",
+  messagingSenderId: "459502546718",
+  appId: "1:459502546718:web:bbf6bea6dc498b7c07a30f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
