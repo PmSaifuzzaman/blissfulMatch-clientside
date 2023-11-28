171257a8cb8650d12634f2721b22e1e5eda44445
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-28 14:29:49 +0600

    Delete functionality added to favourite section

--- src/pages/Dashboard/Favourites/Favourites.jsx
+++ src/pages/Dashboard/Favourites/Favourites.jsx
@@ -1,20 +1,48 @@
+import { Button } from "@material-tailwind/react";
 import useFavourites from "../../../hooks/useFavourites";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 // import FavouriteTable from "./FavouriteTable";
 
 
 const Favourites = () => {
 
-    const [Favourites] = useFavourites()
+    const [Favourites, refetch] = useFavourites()
+    const axiosSecure = useAxiosSecure()
+
+    const handleDelete = (id) => {
+        console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/favourites/${id}`)
+                .then(res => {
+                    console.log(res)
+                    if (res.data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: `Favourite has been deleted.`,
+                            icon: "success"
+                        });
+                    }
+                })
+            }
+          });
+    }
 
     return (
         <div>
-            <h2>My favourites : {Favourites.length}</h2>
-            {/* <div>
-                {
-                    Favourites.map(favourite => <FavouriteTable key={favourite._id} favourite={favourite}></FavouriteTable>)
-                }
-            </div> */}
+            <h2 className="text-center text-3xl mb-3">My favourites : <span className="font-bold text-pink-400">{Favourites.length}</span></h2>
+            
             <div>
                 <table border="1" className="w-full ">
                     <thead className=" bg-blue-gray-200 text-left ">
@@ -30,12 +58,12 @@ const Favourites = () => {
                     <tbody className="text-left">
                         {
                             Favourites.map((favourite , index) => <tr key={favourite._id}>
-                                <td className="py-5 shadow-lg">{index + 1}</td>
+                                <td className="py-5 pl-5 shadow-lg">{index + 1}.</td>
                                 <td className="py-5 shadow-lg">{favourite.Name}</td>
                                 <td className="py-5 shadow-lg">{favourite.BiodataNumber}</td>
                                 <td className="py-5 shadow-lg">{favourite.PermanentDivisionName}</td>
                                 <td className="py-5 shadow-lg">{favourite.Occupation}</td>
-                                <td className="py-5 shadow-lg"><FaTrashAlt /></td>
+                                <td className="py-5 shadow-lg"><Button onClick={() => handleDelete(favourite._id)} className="bg-blue-gray-100 p-3 ml-5 rounded-full text-red-500"><FaTrashAlt /></Button></td>
                                 
                             </tr>)
                         }
