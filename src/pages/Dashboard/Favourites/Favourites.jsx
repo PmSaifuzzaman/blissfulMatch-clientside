b92dba45cbf89c5917da0f35aeb8803046c7ac35
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-28 13:18:44 +0600

    table added for favourite section

--- src/pages/Dashboard/Favourites/Favourites.jsx
+++ src/pages/Dashboard/Favourites/Favourites.jsx
@@ -1,18 +1,47 @@
 import useFavourites from "../../../hooks/useFavourites";
-import FavouriteTable from "./FavouriteTable";
+import { FaTrashAlt } from "react-icons/fa";
+// import FavouriteTable from "./FavouriteTable";
 
 
 const Favourites = () => {
 
-    const[Favourites] = useFavourites()
+    const [Favourites] = useFavourites()
 
     return (
         <div>
             <h2>My favourites : {Favourites.length}</h2>
-            <div>
+            {/* <div>
                 {
                     Favourites.map(favourite => <FavouriteTable key={favourite._id} favourite={favourite}></FavouriteTable>)
                 }
+            </div> */}
+            <div>
+                <table border="1" className="w-full ">
+                    <thead className=" bg-blue-gray-200 text-left ">
+                        <tr className="">
+                            <th className="py-5 pl-5">#</th>
+                            <th className="py-5">Name</th>
+                            <th className="py-5">Biodata Id</th>
+                            <th className="py-5">Permanent Address</th>
+                            <th className="py-5">Occupation</th>
+                            <th className="py-5">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-left">
+                        {
+                            Favourites.map((favourite , index) => <tr key={favourite._id}>
+                                <td className="py-5 shadow-lg">{index + 1}</td>
+                                <td className="py-5 shadow-lg">{favourite.Name}</td>
+                                <td className="py-5 shadow-lg">{favourite.BiodataNumber}</td>
+                                <td className="py-5 shadow-lg">{favourite.PermanentDivisionName}</td>
+                                <td className="py-5 shadow-lg">{favourite.Occupation}</td>
+                                <td className="py-5 shadow-lg"><FaTrashAlt /></td>
+                                
+                            </tr>)
+                        }
+                        
+                    </tbody>
+                </table>
             </div>
         </div>
     );
