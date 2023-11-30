08ab11b5b48966199fa52092d9cbe96ef89b3010
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-11-30 20:20:13 +0600

    Pagination property added to the Biodatas page

--- src/pages/Biodatas/Biodatas.jsx
+++ src/pages/Biodatas/Biodatas.jsx
@@ -2,21 +2,29 @@ import { useEffect, useState } from "react";
 import Navigationbar from "../../components/Navbar/Navbar";
 import BiodataCard from "../shared/BiodataCard/BiodataCard";
 
+
+
 import React from "react";
 import {
     Drawer,
     Button,
     Typography,
     IconButton,
+    Select,
+    Option,
 } from "@material-tailwind/react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 
 const Biodatas = () => {
+    const itemsPerPage = 9;
+
+    const [currentPage, setCurrentPage] = useState(1);
 
     const [biodatas, setBiodatas] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/biodatas")
+        fetch(`http://localhost:5000/biodatas`)
             .then(res => res.json())
             .then(data => setBiodatas(data))
     }, [])
@@ -25,6 +33,39 @@ const Biodatas = () => {
 
     const openDrawer = () => setOpen(true);
     const closeDrawer = () => setOpen(false);
+
+    // For pagination
+    const getItemProps = (index) => ({
+        variant: "text",
+        color: "gray",
+        onClick: () => goToPage(index),
+        className: currentPage === index ? "bg-pink-400 text-white" : "", // Add this line for background color
+    });
+
+    const maxPageCount = Math.ceil(biodatas.length / itemsPerPage);
+
+    const getPageData = () => {
+        const startIndex = (currentPage - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        return biodatas.slice(startIndex, endIndex);
+    };
+
+    const next = () => {
+        if (currentPage < maxPageCount) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const prev = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const goToPage = (page) => {
+        setCurrentPage(page);
+    };
+
 
 
     return (
@@ -38,7 +79,7 @@ const Biodatas = () => {
                     <Drawer open={open} onClose={closeDrawer} className="p-4">
                         <div className="mb-6 flex items-center justify-between">
                             <Typography variant="h5" color="blue-gray">
-                                Material Tailwind
+                                Biodata Filter By
                             </Typography>
                             <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
                                 <svg
@@ -58,22 +99,74 @@ const Biodatas = () => {
                             </IconButton>
                         </div>
                         <Typography color="gray" className="mb-8 pr-4 font-normal">
-                            Material Tailwind features multiple React and HTML components, all
-                            written with Tailwind CSS classes and Material Design guidelines.
+                            <Select label="Filter by age range" >
+                                <Option value="20-25">20-25</Option>
+                                <Option value="25-30">25-30</Option>
+                                <Option value="30-35">30-35</Option>
+                                <Option value="35-40">35-40</Option>
+                                <Option value="40-45">40-45</Option>
+                            </Select>
                         </Typography>
-                        <div className="flex gap-2">
-                            <Button size="sm" variant="outlined">
-                                Documentation
-                            </Button>
-                            <Button size="sm">Get Started</Button>
-                        </div>
+                        <Typography color="gray" className="mb-8 pr-4 font-normal">
+                            <Select label="Filter by type" >
+                                <Option value="Male">Male</Option>
+                                <Option value="Female">Female</Option>
+                            </Select>
+                        </Typography>
+                        <Typography color="gray" className="mb-8 pr-4 font-normal">
+                            <Select label="Filter by Division" >
+                                <Option value="Dhaka">Dhaka</Option>
+                                <Option value="Chittagong">Chittagong</Option>
+                                <Option value="Khulna">Khulna</Option>
+                                <Option value="Rajshahi">Rajshahi</Option>
+                                <Option value="Barisal">Barisal</Option>
+                                <Option value="Sylhet">Sylhet</Option>
+                                <Option value="Rangpur">Rangpur</Option>
+                                <Option value="Mymensing">Mymensing</Option>
+                            </Select>
+                        </Typography>
                     </Drawer>
                 </React.Fragment>
             </div>
             <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+
                 {
-                    biodatas.map(biodata => <BiodataCard key={biodata._id} biodata={biodata}></BiodataCard>)
+                    getPageData().map((biodata) => (<BiodataCard key={biodata._id} biodata={biodata}></BiodataCard>))
                 }
+
+                {/* {
+                    biodatas.map(biodata => <BiodataCard key={biodata._id} biodata={biodata}></BiodataCard>)
+                } */}
+            </div>
+            {/*For pagination  */}
+            <div className="flex items-center justify-center gap-4 my-5">
+                <Button
+                    variant="text"
+                    className="flex items-center gap-2"
+                    onClick={prev}
+                    disabled={currentPage === 1}
+                >
+                    <FaArrowLeft strokeWidth={2} className="h-4 w-4" /> Previous
+                </Button>
+                <div className="flex items-center gap-2">
+                    {[...Array(maxPageCount).keys()].map((page) => (
+                        <IconButton
+                            key={page}
+                            {...getItemProps(page + 1)}
+                            onClick={() => goToPage(page + 1)}
+                        >
+                            {page + 1}
+                        </IconButton>
+                    ))}
+                </div>
+                <Button
+                    variant="text"
+                    className="flex items-center gap-2"
+                    onClick={next}
+                    disabled={currentPage === maxPageCount}
+                >
+                    Next <FaArrowRight strokeWidth={2} className="h-4 w-4" />
+                </Button>
             </div>
 
         </div>
